9dd678c85a71c0254f4affff8ffdef782e93b019
eder-repository <ederzambranomero@gmail.com>
2024-09-07 01:45:35 -0500

    🔧 (bug_report.md): Remove bug report issue template 🔧 (build.md): Remove build system issue template 🔧 (chore.md): Remove chore issue template 🔧 (ci.md): Remove continuous integration issue template 🔧 (config.yml): Remove config.yml file 🔧 (documentation.md): Remove documentation issue template 🔧 (feature_request.md): Remove feature request issue template 🔧 (performance.md): Remove performance update issue template 🔧 (refactor.md): Remove refactor issue template 🔧 (revert.md): Remove revert commit issue template 🔧 (style.md): Remove style changes issue template 🔧 (test.md): Remove test issue template 🔧 (PULL_REQUEST_TEMPLATE.md): Remove pull request template 🔧 (cspell.json): Remove cspell.json file 🔧 (dependabot.yaml): Remove dependabot.yaml file 🔧 (main.yaml): Remove main.yaml workflow file 🔧 (.gitignore): Remove .gitignore file
    📝 (README.md): Remove unnecessary files and update installation instructions and CI information in README.md
    🔧 (analysis_options.yaml): Remove analysis_options.yaml file
    🔧 (coverage_badge.svg): Remove coverage_badge.svg file
    ♻️ (all_legal_ui.dart): Remove unused all_legal_ui.dart file
    ♻️ (all_legal_ui_test.dart): Remove unused all_legal_ui_test.dart file
    ♻️ (pubspec.yaml): Remove unnecessary dependencies and dev_dependencies in pubspec.yaml

--- all_legal_ui/lib/all_legal_ui.dart
+++
@@ -1,4 +0,0 @@
-/// A Very Good Project created by Very Good CLI.
-library;
-
-export 'src/all_legal_ui.dart';
--- all_legal_ui/lib/src/all_legal_ui.dart
+++
@@ -1,7 +0,0 @@
-/// {@template all_legal_ui}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class AllLegalUi {
-  /// {@macro all_legal_ui}
-  const AllLegalUi();
-}
