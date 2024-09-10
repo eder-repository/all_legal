f9d362fc9855caed3f83b79791c71b969701efa5
eder-repository <ederzambranomero@gmail.com>
2024-09-10 00:34:08 -0500

    more: add more change

---
+++ lib/src/features/sign_document/presentation/bloc/bloc/sign_document_bloc.freezed.dart
@@ -0,0 +1,610 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'sign_document_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$SignDocumentEvent {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() savePdf,
+    required TResult Function() getListPdf,
+    required TResult Function(int index) deletePdf,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? savePdf,
+    TResult? Function()? getListPdf,
+    TResult? Function(int index)? deletePdf,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? savePdf,
+    TResult Function()? getListPdf,
+    TResult Function(int index)? deletePdf,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_SavePdf value) savePdf,
+    required TResult Function(_GetListPdf value) getListPdf,
+    required TResult Function(_DeletePdf value) deletePdf,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_SavePdf value)? savePdf,
+    TResult? Function(_GetListPdf value)? getListPdf,
+    TResult? Function(_DeletePdf value)? deletePdf,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_SavePdf value)? savePdf,
+    TResult Function(_GetListPdf value)? getListPdf,
+    TResult Function(_DeletePdf value)? deletePdf,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SignDocumentEventCopyWith<$Res> {
+  factory $SignDocumentEventCopyWith(
+          SignDocumentEvent value, $Res Function(SignDocumentEvent) then) =
+      _$SignDocumentEventCopyWithImpl<$Res, SignDocumentEvent>;
+}
+
+/// @nodoc
+class _$SignDocumentEventCopyWithImpl<$Res, $Val extends SignDocumentEvent>
+    implements $SignDocumentEventCopyWith<$Res> {
+  _$SignDocumentEventCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of SignDocumentEvent
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$SavePdfImplCopyWith<$Res> {
+  factory _$$SavePdfImplCopyWith(
+          _$SavePdfImpl value, $Res Function(_$SavePdfImpl) then) =
+      __$$SavePdfImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$SavePdfImplCopyWithImpl<$Res>
+    extends _$SignDocumentEventCopyWithImpl<$Res, _$SavePdfImpl>
+    implements _$$SavePdfImplCopyWith<$Res> {
+  __$$SavePdfImplCopyWithImpl(
+      _$SavePdfImpl _value, $Res Function(_$SavePdfImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of SignDocumentEvent
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$SavePdfImpl implements _SavePdf {
+  const _$SavePdfImpl();
+
+  @override
+  String toString() {
+    return 'SignDocumentEvent.savePdf()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$SavePdfImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() savePdf,
+    required TResult Function() getListPdf,
+    required TResult Function(int index) deletePdf,
+  }) {
+    return savePdf();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? savePdf,
+    TResult? Function()? getListPdf,
+    TResult? Function(int index)? deletePdf,
+  }) {
+    return savePdf?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? savePdf,
+    TResult Function()? getListPdf,
+    TResult Function(int index)? deletePdf,
+    required TResult orElse(),
+  }) {
+    if (savePdf != null) {
+      return savePdf();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_SavePdf value) savePdf,
+    required TResult Function(_GetListPdf value) getListPdf,
+    required TResult Function(_DeletePdf value) deletePdf,
+  }) {
+    return savePdf(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_SavePdf value)? savePdf,
+    TResult? Function(_GetListPdf value)? getListPdf,
+    TResult? Function(_DeletePdf value)? deletePdf,
+  }) {
+    return savePdf?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_SavePdf value)? savePdf,
+    TResult Function(_GetListPdf value)? getListPdf,
+    TResult Function(_DeletePdf value)? deletePdf,
+    required TResult orElse(),
+  }) {
+    if (savePdf != null) {
+      return savePdf(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _SavePdf implements SignDocumentEvent {
+  const factory _SavePdf() = _$SavePdfImpl;
+}
+
+/// @nodoc
+abstract class _$$GetListPdfImplCopyWith<$Res> {
+  factory _$$GetListPdfImplCopyWith(
+          _$GetListPdfImpl value, $Res Function(_$GetListPdfImpl) then) =
+      __$$GetListPdfImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$GetListPdfImplCopyWithImpl<$Res>
+    extends _$SignDocumentEventCopyWithImpl<$Res, _$GetListPdfImpl>
+    implements _$$GetListPdfImplCopyWith<$Res> {
+  __$$GetListPdfImplCopyWithImpl(
+      _$GetListPdfImpl _value, $Res Function(_$GetListPdfImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of SignDocumentEvent
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$GetListPdfImpl implements _GetListPdf {
+  const _$GetListPdfImpl();
+
+  @override
+  String toString() {
+    return 'SignDocumentEvent.getListPdf()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$GetListPdfImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() savePdf,
+    required TResult Function() getListPdf,
+    required TResult Function(int index) deletePdf,
+  }) {
+    return getListPdf();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? savePdf,
+    TResult? Function()? getListPdf,
+    TResult? Function(int index)? deletePdf,
+  }) {
+    return getListPdf?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? savePdf,
+    TResult Function()? getListPdf,
+    TResult Function(int index)? deletePdf,
+    required TResult orElse(),
+  }) {
+    if (getListPdf != null) {
+      return getListPdf();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_SavePdf value) savePdf,
+    required TResult Function(_GetListPdf value) getListPdf,
+    required TResult Function(_DeletePdf value) deletePdf,
+  }) {
+    return getListPdf(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_SavePdf value)? savePdf,
+    TResult? Function(_GetListPdf value)? getListPdf,
+    TResult? Function(_DeletePdf value)? deletePdf,
+  }) {
+    return getListPdf?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_SavePdf value)? savePdf,
+    TResult Function(_GetListPdf value)? getListPdf,
+    TResult Function(_DeletePdf value)? deletePdf,
+    required TResult orElse(),
+  }) {
+    if (getListPdf != null) {
+      return getListPdf(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _GetListPdf implements SignDocumentEvent {
+  const factory _GetListPdf() = _$GetListPdfImpl;
+}
+
+/// @nodoc
+abstract class _$$DeletePdfImplCopyWith<$Res> {
+  factory _$$DeletePdfImplCopyWith(
+          _$DeletePdfImpl value, $Res Function(_$DeletePdfImpl) then) =
+      __$$DeletePdfImplCopyWithImpl<$Res>;
+  @useResult
+  $Res call({int index});
+}
+
+/// @nodoc
+class __$$DeletePdfImplCopyWithImpl<$Res>
+    extends _$SignDocumentEventCopyWithImpl<$Res, _$DeletePdfImpl>
+    implements _$$DeletePdfImplCopyWith<$Res> {
+  __$$DeletePdfImplCopyWithImpl(
+      _$DeletePdfImpl _value, $Res Function(_$DeletePdfImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of SignDocumentEvent
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? index = null,
+  }) {
+    return _then(_$DeletePdfImpl(
+      null == index
+          ? _value.index
+          : index // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$DeletePdfImpl implements _DeletePdf {
+  const _$DeletePdfImpl(this.index);
+
+  @override
+  final int index;
+
+  @override
+  String toString() {
+    return 'SignDocumentEvent.deletePdf(index: $index)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$DeletePdfImpl &&
+            (identical(other.index, index) || other.index == index));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, index);
+
+  /// Create a copy of SignDocumentEvent
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$DeletePdfImplCopyWith<_$DeletePdfImpl> get copyWith =>
+      __$$DeletePdfImplCopyWithImpl<_$DeletePdfImpl>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() savePdf,
+    required TResult Function() getListPdf,
+    required TResult Function(int index) deletePdf,
+  }) {
+    return deletePdf(index);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? savePdf,
+    TResult? Function()? getListPdf,
+    TResult? Function(int index)? deletePdf,
+  }) {
+    return deletePdf?.call(index);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? savePdf,
+    TResult Function()? getListPdf,
+    TResult Function(int index)? deletePdf,
+    required TResult orElse(),
+  }) {
+    if (deletePdf != null) {
+      return deletePdf(index);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_SavePdf value) savePdf,
+    required TResult Function(_GetListPdf value) getListPdf,
+    required TResult Function(_DeletePdf value) deletePdf,
+  }) {
+    return deletePdf(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_SavePdf value)? savePdf,
+    TResult? Function(_GetListPdf value)? getListPdf,
+    TResult? Function(_DeletePdf value)? deletePdf,
+  }) {
+    return deletePdf?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_SavePdf value)? savePdf,
+    TResult Function(_GetListPdf value)? getListPdf,
+    TResult Function(_DeletePdf value)? deletePdf,
+    required TResult orElse(),
+  }) {
+    if (deletePdf != null) {
+      return deletePdf(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _DeletePdf implements SignDocumentEvent {
+  const factory _DeletePdf(final int index) = _$DeletePdfImpl;
+
+  int get index;
+
+  /// Create a copy of SignDocumentEvent
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$DeletePdfImplCopyWith<_$DeletePdfImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+mixin _$SignDocumentState {
+  List<PdfEntitie> get pdfs => throw _privateConstructorUsedError;
+  PdfStatus get status => throw _privateConstructorUsedError;
+
+  /// Create a copy of SignDocumentState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $SignDocumentStateCopyWith<SignDocumentState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SignDocumentStateCopyWith<$Res> {
+  factory $SignDocumentStateCopyWith(
+          SignDocumentState value, $Res Function(SignDocumentState) then) =
+      _$SignDocumentStateCopyWithImpl<$Res, SignDocumentState>;
+  @useResult
+  $Res call({List<PdfEntitie> pdfs, PdfStatus status});
+}
+
+/// @nodoc
+class _$SignDocumentStateCopyWithImpl<$Res, $Val extends SignDocumentState>
+    implements $SignDocumentStateCopyWith<$Res> {
+  _$SignDocumentStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of SignDocumentState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? pdfs = null,
+    Object? status = null,
+  }) {
+    return _then(_value.copyWith(
+      pdfs: null == pdfs
+          ? _value.pdfs
+          : pdfs // ignore: cast_nullable_to_non_nullable
+              as List<PdfEntitie>,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as PdfStatus,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$SignDocumentStateImplCopyWith<$Res>
+    implements $SignDocumentStateCopyWith<$Res> {
+  factory _$$SignDocumentStateImplCopyWith(_$SignDocumentStateImpl value,
+          $Res Function(_$SignDocumentStateImpl) then) =
+      __$$SignDocumentStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({List<PdfEntitie> pdfs, PdfStatus status});
+}
+
+/// @nodoc
+class __$$SignDocumentStateImplCopyWithImpl<$Res>
+    extends _$SignDocumentStateCopyWithImpl<$Res, _$SignDocumentStateImpl>
+    implements _$$SignDocumentStateImplCopyWith<$Res> {
+  __$$SignDocumentStateImplCopyWithImpl(_$SignDocumentStateImpl _value,
+      $Res Function(_$SignDocumentStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of SignDocumentState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? pdfs = null,
+    Object? status = null,
+  }) {
+    return _then(_$SignDocumentStateImpl(
+      pdfs: null == pdfs
+          ? _value._pdfs
+          : pdfs // ignore: cast_nullable_to_non_nullable
+              as List<PdfEntitie>,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as PdfStatus,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$SignDocumentStateImpl implements _SignDocumentState {
+  const _$SignDocumentStateImpl(
+      {final List<PdfEntitie> pdfs = const [], this.status = PdfStatus.initial})
+      : _pdfs = pdfs;
+
+  final List<PdfEntitie> _pdfs;
+  @override
+  @JsonKey()
+  List<PdfEntitie> get pdfs {
+    if (_pdfs is EqualUnmodifiableListView) return _pdfs;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_pdfs);
+  }
+
+  @override
+  @JsonKey()
+  final PdfStatus status;
+
+  @override
+  String toString() {
+    return 'SignDocumentState(pdfs: $pdfs, status: $status)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$SignDocumentStateImpl &&
+            const DeepCollectionEquality().equals(other._pdfs, _pdfs) &&
+            (identical(other.status, status) || other.status == status));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, const DeepCollectionEquality().hash(_pdfs), status);
+
+  /// Create a copy of SignDocumentState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$SignDocumentStateImplCopyWith<_$SignDocumentStateImpl> get copyWith =>
+      __$$SignDocumentStateImplCopyWithImpl<_$SignDocumentStateImpl>(
+          this, _$identity);
+}
+
+abstract class _SignDocumentState implements SignDocumentState {
+  const factory _SignDocumentState(
+      {final List<PdfEntitie> pdfs,
+      final PdfStatus status}) = _$SignDocumentStateImpl;
+
+  @override
+  List<PdfEntitie> get pdfs;
+  @override
+  PdfStatus get status;
+
+  /// Create a copy of SignDocumentState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$SignDocumentStateImplCopyWith<_$SignDocumentStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
